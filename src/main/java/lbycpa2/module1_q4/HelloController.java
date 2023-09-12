f0d290688dae5df543f28fde8e73674ed3255452
Alawnely <15603444+Alawnely@users.noreply.github.com>
2023-09-12 09:12:19 +0800

    Initial commit

---
+++ src/main/java/lbycpa2/module1_q4/HelloController.java
@@ -0,0 +1,14 @@
+package lbycpa2.module1_q4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lbycpa2.module1_q4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lbycpa2.module1_q4 to javafx.fxml;
+    exports lbycpa2.module1_q4;
+}
