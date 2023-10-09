99186f38ec7dfc66dd95720b8066ff8079883f75
Alawnely <15603444+Alawnely@users.noreply.github.com>
2023-10-09 23:22:35 +0800

    add transition ;)

--- src/main/java/lbycpa2/module4/QuestionController.java
+++ src/main/java/lbycpa2/module4/QuestionController.java
@@ -1,8 +1,20 @@
 package lbycpa2.module4;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.util.LinkedList;
 
@@ -34,6 +46,9 @@ public class QuestionController {
     }
 
     protected void setQuestion(Question question) {
+        Scene mainScene = MainApplication.getWindow().getScene();
+        WritableImage snapshot = mainScene.snapshot(null);
+
         quesCount++;
         currQuestion = question;
         quesNum.setText("Question "+quesCount);
@@ -43,6 +58,34 @@ public class QuestionController {
         choice2.setText(choices[1]);
         choice3.setText(choices[2]);
         choice4.setText(choices[3]);
+
+        transitionAway(snapshot);
+    }
+
+    private void transitionAway(WritableImage snapshot) {
+        Scene mainScene = MainApplication.getWindow().getScene();
+        Parent root = mainScene.getRoot();
+        System.out.println(root);
+
+        if (root instanceof StackPane) {
+            return;
+        }
+
+        StackPane transitionPane = new StackPane();
+        transitionPane.setMinWidth(mainScene.getWidth());
+        transitionPane.setMinHeight(mainScene.getHeight());
+
+        ImageView snapshotImage = new ImageView(snapshot);
+        transitionPane.getChildren().addAll(root, snapshotImage);
+        mainScene.setRoot(transitionPane);
+
+        snapshotImage.translateXProperty().set(0);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(300), new KeyValue(snapshotImage.translateXProperty(), -transitionPane.getWidth(), Interpolator.EASE_IN)));
+        timeline.setOnFinished(e -> {
+            transitionPane.getChildren().remove(root);
+            mainScene.setRoot(root);
+        });
+        timeline.play();
     }
 
     @FXML
