c7278213f8406d0dd7a7a618bd37000a098b5f67
Alawnely <15603444+Alawnely@users.noreply.github.com>
2023-10-27 01:16:42 +0800

    javafx bruh moments

---
+++ src/main/java/lbycpa2/module4/MainStarter.java
@@ -0,0 +1,8 @@
+package lbycpa2.module4;
+
+// https://stackoverflow.com/a/58498686
+public class MainStarter {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
