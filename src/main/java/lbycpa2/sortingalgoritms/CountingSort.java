8c1122f5b7d865de3c0d9d25cf2fdd8930892dd4
ajhinri <140769541+ajhinri@users.noreply.github.com>
2023-11-23 05:50:21 +0800

    added countingsort wip

---
+++ src/main/java/lbycpa2/sortingalgoritms/CountingSort.java
@@ -0,0 +1,47 @@
+package lbycpa2.sortingalgoritms;
+
+public class CountingSort {
+    //TODO: Convert to CCountingSort
+
+    public static void countingSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        // Find the maximum value in the array to determine the range of values
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        // Create a counting array to store the count of each element
+        int[] countArray = new int[max + 1];
+
+        // Count the occurrences of each element
+        for (int num : arr) {
+            countArray[num]++;
+        }
+
+        // Reconstruct the sorted array
+        int index = 0;
+        for (int i = 0; i <= max; i++) {
+            while (countArray[i] > 0) {
+                arr[index] = i;
+                index++;
+                countArray[i]--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 2, 8, 3, 3, 1};
+        countingSort(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
